7eac19da56a5aa370ff0185a794170bedc096b17
Satyabrat Srikumar <satyabrat.srikumar@columbia.edu>
2026-03-11 01:27:00 -0400

    mounted static webpage on fastapi and updated endpoints

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 import sqlite3
 
 app = FastAPI()
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.add_middleware(
     CORSMiddleware,
@@ -13,13 +15,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/")
 @app.get("/papers")
 def get_papers():
-    conn = sqlite3.connect('papers.db')
-    cur = conn.cursor()
-    cur.row_factory = sqlite3.Row
-    cur.execute("SELECT * FROM papers")
-    rows = cur.fetchall()
-    conn.close()
-    return {"papers": [dict(row) for row in rows]}
-
+    # conn = sqlite3.connect('papers.db')
+    # cur = conn.cursor()
+    # cur.row_factory = sqlite3.Row
+    # cur.execute("SELECT * FROM papers")
+    # rows = cur.fetchall()
+    # conn.close()
+    # return {"papers": [dict(row) for row in rows]}
+    return FileResponse("static/index.html")
